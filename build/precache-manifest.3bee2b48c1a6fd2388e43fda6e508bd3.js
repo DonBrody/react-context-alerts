8aab921fdef23c251f9844d24adeca652c7a3805
Don Brody <donald.brody@gmail.com>
2018-11-24 17:10:25 -0500

    Revert react-scripts to 2.0.5

---
+++ build/precache-manifest.3bee2b48c1a6fd2388e43fda6e508bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98e5817e296afe171241",
+    "url": "/DonBrody/react-context-alerts/static/js/runtime~main.98e5817e.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/DonBrody/react-context-alerts/static/js/main.0dc81eb4.chunk.js"
+  },
+  {
+    "revision": "5ce4a0d64a5cc40a80b1",
+    "url": "/DonBrody/react-context-alerts/static/js/1.5ce4a0d6.chunk.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/DonBrody/react-context-alerts/static/css/main.4631faf2.chunk.css"
+  },
+  {
+    "revision": "d7cfbbb034b6b605bd4298b2f5593d53",
+    "url": "/DonBrody/react-context-alerts/index.html"
+  }
+];
