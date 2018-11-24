331f39f9967b3d88b150b43aa8ec0863a4910b59
Don Brody <donald.brody@gmail.com>
2018-11-24 17:20:50 -0500

    Additional build files

---
+++ build/precache-manifest.e8c702336c72072dc7799c57bebe3c3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/static/js/main.0dc81eb4.chunk.js"
+  },
+  {
+    "revision": "5ce4a0d64a5cc40a80b1",
+    "url": "/static/js/1.5ce4a0d6.chunk.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/static/css/main.4631faf2.chunk.css"
+  },
+  {
+    "revision": "18c23c3bbf059ac183f139366616c136",
+    "url": "/index.html"
+  }
+];
