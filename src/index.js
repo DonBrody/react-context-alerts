aa54b1fa47fed3f8d2de134b816a7f844e8067c4
Don Brody <donald.brody@gmail.com>
2018-11-24 16:00:06 -0500

    Move index.css to src root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './example/App';
-import './example/index.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
