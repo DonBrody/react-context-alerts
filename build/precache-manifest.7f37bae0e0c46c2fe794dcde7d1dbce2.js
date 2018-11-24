49075cec888ab52e0467ccda306421618ad69848
Don Brody <donald.brody@gmail.com>
2018-11-24 16:59:41 -0500

    Built for Heroku deployment

---
+++ build/precache-manifest.7f37bae0e0c46c2fe794dcde7d1dbce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98e5817e296afe171241",
+    "url": "/DonBrody/react-context-alerts/static/js/runtime~main.98e5817e.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/DonBrody/react-context-alerts/static/js/main.0dc81eb4.chunk.js"
+  },
+  {
+    "revision": "5ce4a0d64a5cc40a80b1",
+    "url": "/DonBrody/react-context-alerts/static/js/1.5ce4a0d6.chunk.js"
+  },
+  {
+    "revision": "0dc81eb42b5f76d56dec",
+    "url": "/DonBrody/react-context-alerts/static/css/main.4631faf2.chunk.css"
+  },
+  {
+    "revision": "c6c3ca44f3a102f72e551c83f2c4aba0",
+    "url": "/DonBrody/react-context-alerts/index.html"
+  }
+];
