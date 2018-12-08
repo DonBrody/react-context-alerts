2334e5d033b531b627324ff35da24ac3c9889fea
Don Brody <donald.brody@gmail.com>
2018-12-08 17:14:14 -0500

    Setup new app project with dependencies and existing code

--- example/rca-demo/src/App.js
+++ example/rca-demo/src/App.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AlertsProvider, createRcaTheme, createRcaSettings } from 'react-context-alerts';
+import Main from './views/main';
+
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: { main: '#2196F3' },
+    secondary: { main: '#FFF' },
+    info: { main: '#1976d2', darker: '#0d47a1' },
+    success: { main: '#388e3c', darker: '#1b5e20' },
+    warning: { main: '#f57c00', darker: '#e65100' },
+    error: { main: '#d32f2f', darker: '#b71c1c' },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+const rcaTheme = createRcaTheme({
+});
+
+const rcaSettings = createRcaSettings({
+});
+
+const rcaStyles = {
+};
+
+const AppBase = () => (
+  <Redirect to ="/" />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={muiTheme}>
+        <div>
+          <header role="banner">
+            <AppBar position="static" color="primary">
+              <Toolbar>
+                <Typography variant="h5" component="h1" color="secondary">
+                  React Context Alerts Demo
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </header>
+          <AlertsProvider theme={rcaTheme} settings={rcaSettings} style={rcaStyles}>
+            <main role="main">
+              <BrowserRouter>
+                <Switch>
+                  <Route exact path="/" component={Main}/>
+                  <Route path="/" component={AppBase} />
+                </Switch>
+              </BrowserRouter>
+            </main>
+          </AlertsProvider>
+          <footer style={{ marginTop: 60, textAlign: 'center' }}>
+            <Typography variant="caption" component="aside">
+              MIT Licensed Software
+            </Typography>
+          </footer>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
