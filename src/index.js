9dde9d73f48288d1fdedf6613943ae3925398bf7
Don Brody <donald.brody@gmail.com>
2018-12-08 19:57:40 -0500

    Create development playground for shared components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './playground/App';
+import './playground/index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
