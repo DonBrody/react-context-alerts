cbef9c0821d8479c676293a2384dac88d278ae0f
Don Brody <donald.brody@gmail.com>
2018-12-19 21:10:19 -0500

    Add Alert to TypeScript definition index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 export { default as createRcaTheme } from './theme/createRcaTheme';
 export { default as createRcaSettings } from './settings/createRcaSettings';
 export { default as AlertsProvider, AlertsConsumer, AlertsContext } from './context/AlertsProvider';
+export { default as Alert } from './components/Alert';
 export { default as RcaTheme } from './theme';
 export { default as RcaSettings } from './settings';
