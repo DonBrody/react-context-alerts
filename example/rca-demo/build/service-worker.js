a7e74957655efa13cd3bb7d7c8b1e2f6c908c611
Don Brody <donald.brody@gmail.com>
2019-02-14 15:45:47 -0500

    Update example project to latest lib

--- example/rca-demo/build/service-worker.js
+++ example/rca-demo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fc6a6c9d9393b319e301164c342e44c.js"
+  "/precache-manifest.49dc504c8de613c48bf456f34898dc9b.js"
 );
 
 workbox.clientsClaim();
