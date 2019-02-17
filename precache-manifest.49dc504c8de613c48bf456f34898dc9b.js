4b6c46993723641f474d7db9af196d717e08e558
Don Brody <donald.brody@gmail.com>
2019-02-17 08:41:37 -0500

    Updates

---
+++ precache-manifest.49dc504c8de613c48bf456f34898dc9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1b7fd5eef6664bb3be34",
+    "url": "/static/js/main.1b7fd5ee.chunk.js"
+  },
+  {
+    "revision": "e1fb7d28c512d71169b6",
+    "url": "/static/js/2.e1fb7d28.chunk.js"
+  },
+  {
+    "revision": "1b7fd5eef6664bb3be34",
+    "url": "/static/css/main.8d756571.chunk.css"
+  },
+  {
+    "revision": "1f159772a274b38d4e8108f135bc311c",
+    "url": "/index.html"
+  }
+];
