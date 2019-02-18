aad7cec1013ad9ddc6c99b6804b2f4dbe46183df
Don Brody <donald.brody@gmail.com>
2019-02-17 19:40:24 -0500

    Updates

---
+++ precache-manifest.db627492d0b13153905a04175d7cdfa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7ba6fdfecc838cc10b",
+    "url": "/react-context-alerts/static/js/runtime~main.bf7ba6fd.js"
+  },
+  {
+    "revision": "fb54c560d2103cd4e716",
+    "url": "/react-context-alerts/static/js/main.fb54c560.chunk.js"
+  },
+  {
+    "revision": "2c5d6913ccecf681ef0a",
+    "url": "/react-context-alerts/static/js/2.2c5d6913.chunk.js"
+  },
+  {
+    "revision": "fb54c560d2103cd4e716",
+    "url": "/react-context-alerts/static/css/main.8d756571.chunk.css"
+  },
+  {
+    "revision": "29e3c80d3868c4c96d934528055730c6",
+    "url": "/react-context-alerts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e16501c40a134be1b5992c85e44edc1b.js"
+  "/react-context-alerts/precache-manifest.db627492d0b13153905a04175d7cdfa1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-context-alerts/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
